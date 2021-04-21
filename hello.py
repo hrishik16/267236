f0b3420c7c796da57c0449c3a545356326f5e1b8
HRISHI KHOLAMKAR <hkholamkar16@gmail.com>
2021-04-21 13:44:41 +0530

    Fourth commit

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
+#................printing hello world in python.......
 print("Hello")
+#.....Input output in python...
 a=int(input())
 b=int(input())
 print(a+b)
-lst=[1,2,3,4,5]
-for i in lst:
-    print(i,end=" ")
+#......Begining with List in Python.........
+lst=[]
+for i in range(10):
+    lst.append(i)
+    
