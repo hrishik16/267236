e6898c6c30eb05f126833c4406100a7cf48275a5
HRISHI KHOLAMKAR <hkholamkar16@gmail.com>
2021-04-22 18:50:19 +0530

    Eightth commit

---
+++ If_Elif Unsolved/InputAbbrevation.py
@@ -0,0 +1,11 @@
+s=input()
+if(s=='lol'):
+    print("laughing out loud")
+elif(s=='rofl'):
+    print("rolling on the floor")
+elif(s=='lmk'):
+    print("let me know")
+elif(s=='smh'):
+    print("shaking my hand")
+else:
+    print("Wrong input")
